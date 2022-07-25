82f2ee6d298a8ec8a07ffeeae95b2f23f8280b2d
Almond Chang <almondchang0629@gmail.com>
2022-07-25 20:33:50 +0800

    Saved best models

--- K% Training (Linear).py
+++ K% Training (Linear).py
@@ -1,34 +1,46 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from math import sqrt
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split
+import pickle
 
-train_df = pd.read_excel('CSV_files/K_savant.xlsx', sheet_name='Training')
-df = pd.read_excel('CSV_files/K_savant.xlsx', sheet_name='Testing')
+# functions
+def save_model(regr):
+    filename = 'Best Models/Linear (85:15).pkl'
+    pickle.dump(regr, open(filename, 'wb')) #Save the model
+    
+prev_RMSE = 10
+full_df = pd.read_csv('CSV_files/K%_data.csv')
+result_df = pd.DataFrame(data={},columns=["MAE", "RMSE", "R2"])
+for j in range(0, 2000):
+    train_df, test_df = train_test_split(full_df, test_size=0.15)
+    X = train_df[["O-Swing%", "O-Contact%", "Z-Swing%", "Z-Contact%", "Zone%", "F-Strike%", "SwStr%", "CSW%", "M-Swing%", "Meat%", "Edge%","Fair/Foul ratio"]]
+    y = train_df["K%"]
+    regr = linear_model.LinearRegression()
+    regr.fit(X.values, y)
 
-X = train_df[["O-Swing%", "O-Contact%", "Z-Swing%", "Z-Contact%", "Zone%", "F-Strike%", "SwStr%", "CSW%", "M-Swing%", "Meat%", "Edge%","Fair/Foul ratio"]]
-y = train_df["K%"]
-regr = linear_model.LinearRegression()
-regr.fit(X.values, y)
+    regr_df = pd.DataFrame(data={},columns=["Season","Name","xK%","K%"])
+    for i in range(0, test_df.shape[0]): # iterate thru all players, shape[0]: the row count of df
+        predictK = regr.predict([test_df.iloc[i][7:19]]) # feed in required data
+        pid = test_df.iat[i,0]
+        xK = round(predictK[0]*100,3)
+        K = round(test_df.at[pid,'K%']*100,2)
+        row = [test_df.at[pid,'Season'],test_df.at[pid,'Name'],xK,K]
+        regr_df.loc[pid] = row
 
-p = 170
-total_deviation = 0
-regr_df = pd.DataFrame(data={},columns=["Season","Name","xK%","K%","Deviation","Deviation%"])
-for i in range(0, p): # iterate thru all players
-    predictK = regr.predict([df.iloc[i][7:19]]) # feed in required data
-    xK = round(predictK[0]*100,3)
-    K = round(df.at[i,'K%']*100,2)
-    deviation = round((xK-K),3) # the difference between x and real rate
-    deviation_r = round((xK-K)/K*100,3) # the percentage of deviation (deviation value/real stat)
-    row = [df.at[i,'Season'],df.at[i,'Name'],xK,K,deviation,deviation_r]
-    regr_df.loc[i] = row
-    total_deviation = total_deviation + deviation if deviation >= 0 else total_deviation - deviation
+    K_list, xK_list = regr_df["K%"].to_list(),regr_df["xK%"].to_list()
+    RMSE = sqrt(mean_squared_error(K_list, xK_list))
+    MAE = mean_absolute_error(K_list, xK_list)
+    R2 = r2_score(K_list, xK_list)
+    result_row = [MAE,RMSE,R2]
+    result_df.loc[j] = result_row
+    if RMSE < prev_RMSE:
+        save_model(regr)
+        print (RMSE)
+        prev_RMSE = RMSE
 
-average_deviation = total_deviation/p
-K_list, xK_list = regr_df["K%"].to_list(),regr_df["xK%"].to_list()
-MSE = mean_squared_error(K_list, xK_list)
-R2 = r2_score(K_list, xK_list)
-print (f"Average Deviation: {average_deviation}% MSE: {MSE}  R2: {R2}")
-print(regr_df)
-regr_df.to_csv("linear_testing.csv")
+print(result_df)
+result_df.to_csv("Linear_results.csv")
--- Stat converter.py
+++ Stat converter.py
@@ -4,20 +4,20 @@ from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 
 
-df = pd.read_csv('CSV_files/K%_training.csv')
+df = pd.read_csv('CSV_files/K_savant.csv')
 
-for i in range(0, 1450): #1450
-    Pitches = float(df.iat[i, 15])
-    Swing_pct = float(df.iat[i, 16])
-    Contact_pct = float(df.iat[i, 17])
+for i in range(850, 1238): #1450
+    Pitches = float(df.iat[i, 19])
+    Swing_pct = float(df.iat[i, 20])
+    Contact_pct = float(df.iat[i, 21])
     Contacted_Balls = round(Pitches*Swing_pct*Contact_pct)
-    GB = float(df.iat[i, 18])
-    FB = float(df.iat[i, 19])
-    LD = float(df.iat[i, 20])
+    GB = float(df.iat[i, 22])
+    FB = float(df.iat[i, 23])
+    LD = float(df.iat[i, 24])
     Events = GB+FB+LD
     Fouls = Contacted_Balls-Events
     Fair_Foul_ratio = (Events/Contacted_Balls)/(Fouls/Contacted_Balls)
     print (Fair_Foul_ratio)
     df.at[i, "Fair/Foul ratio"] = Fair_Foul_ratio
-    df.to_csv('CSV_files/K%.csv')
+    df.to_csv('CSV_files/K_savant.csv')
 
