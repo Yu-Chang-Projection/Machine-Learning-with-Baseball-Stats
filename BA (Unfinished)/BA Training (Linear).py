1ff97629dae03630a2fee471d1f603d0fc3e069d
Almond Chang <almondchang0629@gmail.com>
2022-08-07 22:13:17 +0800

    HR training

---
+++ BA (Unfinished)/BA Training (Linear).py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from math import sqrt
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.model_selection import train_test_split
+import pickle
+
+# functions
+def save_model(regr):
+    filename = '../Best_Models/Linear_BA_(10).pkl'
+    pickle.dump(regr, open(filename, 'wb'))  # Save the model
+
+prev_RMSE = 10
+full_df = pd.read_csv('../CSV_files/BA_data.csv')
+result_df = pd.DataFrame(data={}, columns=["MAE", "RMSE", "R2"])
+for j in range(20):
+    train_df, test_df = train_test_split(full_df, test_size=0.10)
+    X = train_df[["LD%","GB%","FB%","HR/FB","xBA","xBACON","SweetSpot%","Barrel%","HardHit%"]]
+    y = train_df["BABIP"]
+    regr = linear_model.LinearRegression()
+    regr.fit(X.values, y)
+
+    regr_df = pd.DataFrame(data={}, columns=["Season", "Name", "xBABIP", "BABIP"])
+    for i in range(0, test_df.shape[0]):  # iterate thru all players, shape[0]: the row count of df
+        predictBA = regr.predict([test_df.iloc[i][9:18]])  # feed in required data
+        pid = test_df.iat[i, 0]
+        xBA = round(predictBA[0]*100, 3)
+        BA = round(test_df.at[pid, 'BABIP']*100, 2)
+        row = [test_df.at[pid, 'Season'], test_df.at[pid, 'Name'], xBA, BA]
+        regr_df.loc[pid] = row
+
+    BA_list, xBA_list = regr_df["BABIP"].to_list(), regr_df["xBABIP"].to_list()
+    RMSE = sqrt(mean_squared_error(BA_list, xBA_list))
+    MAE = mean_absolute_error(BA_list, xBA_list)
+    R2 = r2_score(BA_list, xBA_list)
+    result_row = [MAE, RMSE, R2]
+    result_df.loc[j] = result_row
+    if RMSE < prev_RMSE:
+        save_model(regr)
+        print(RMSE)
+        prev_RMSE = RMSE
+
+print(result_df)
+result_df.to_csv("Linear_BA.csv")
--- HR/HR Training (Linear).py
+++ HR/HR Training (Linear).py
@@ -9,38 +9,38 @@ import pickle
 
 # functions
 def save_model(regr):
-    filename = 'best_model.pkl'
+    filename = '../Best_Models/Linear_HR_(15).pkl'
     pickle.dump(regr, open(filename, 'wb'))  # Save the model
 
-prev_RMSE = 100
-full_df = pd.read_csv('../CSV_files/K%_data.csv')
+prev_RMSE = 10
+full_df = pd.read_csv('../CSV_files/HR_data.csv')
 result_df = pd.DataFrame(data={}, columns=["MAE", "RMSE", "R2"])
-for j in range(50):
+for j in range(3000):
     train_df, test_df = train_test_split(full_df, test_size=0.15)
-    X = train_df[["O-Swing%", "O-Contact%", "Z-Swing%", "Z-Contact%", "Zone%", "F-Strike%", "SwStr%", "CSW%", "M-Swing%", "Meat%", "Edge%", "Fair/Foul ratio"]]
-    y = train_df["K%"]
+    X = train_df[["FB%", "LD%", "HR/FB", "Pull%", "Barrel%", "HardHit%"]]
+    y = train_df["HR%"]
     regr = linear_model.LinearRegression()
     regr.fit(X.values, y)
-
-    regr_df = pd.DataFrame(data={}, columns=["Season", "Name", "xK%", "K%"])
+    regr_df = pd.DataFrame(data={}, columns=["Season", "Name", "xHR%", "HR%"])
     for i in range(0, test_df.shape[0]):  # iterate thru all players, shape[0]: the row count of df
-        predictK = regr.predict([test_df.iloc[i][7:19]])  # feed in required data
+        predictHR = regr.predict([test_df.iloc[i][7:13]])  # feed in required data
         pid = test_df.iat[i, 0]
-        xK = round(predictK[0] * 100, 3)
-        K = round(test_df.at[pid, 'K%'] * 100, 2)
-        row = [test_df.at[pid, 'Season'], test_df.at[pid, 'Name'], xK, K]
+        xHR = round(predictHR[0]*10,3)
+        HR = round(test_df.at[pid, 'HR%']*10,2)
+        row = [test_df.at[pid, 'Season'], test_df.at[pid, 'Name'], xHR, HR]
         regr_df.loc[pid] = row
 
-    K_list, xK_list = regr_df["K%"].to_list(), regr_df["xK%"].to_list()
-    RMSE = sqrt(mean_squared_error(K_list, xK_list))
-    MAE = mean_absolute_error(K_list, xK_list)
-    R2 = r2_score(K_list, xK_list)
+    HR_list, xHR_list = regr_df["HR%"].to_list(), regr_df["xHR%"].to_list()
+    RMSE = sqrt(mean_squared_error(HR_list, xHR_list))
+    MAE = mean_absolute_error(HR_list, xHR_list)
+    R2 = r2_score(HR_list, xHR_list)
     result_row = [MAE, RMSE, R2]
     result_df.loc[j] = result_row
     if RMSE < prev_RMSE:
         save_model(regr)
         print(RMSE)
         prev_RMSE = RMSE
+        regr_df.to_csv("Linear_every.csv")
 
 print(result_df)
-result_df.to_csv("result")
+result_df.to_csv("Linear_HR.csv")
